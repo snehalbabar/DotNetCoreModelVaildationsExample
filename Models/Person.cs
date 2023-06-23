d35f944e8b5aa7af2f0e86cca3a0e300cec06d37
snehalbabar <h728e344@wichita.edu>
2023-06-22 20:01:07 -0500

    added some validations

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidationExample.Models
@@ -6,15 +7,20 @@ namespace ModelValidationExample.Models
 	public class Person
 	{
 		[Required]
+		[Display(Name = "person Name")]
+		[StringLength(10, MinimumLength = 2, ErrorMessage = "Not correct Name format")]
 		public string? PersonName { get; set; }
 
 		[Required]
+		[EmailAddress(ErrorMessage = "Not Vaild email address")]
 		public string? Email { get; set; }
 
-		public  string? Phone { get; set; }
+		[Phone(ErrorMessage = "Invalid {0} Number Format")]
+		public string? Phone { get; set; }
 
 		public string? Password { get; set; }
 
+		[Compare("Password", ErrorMessage ="Password not match")]
 		public string? ComfirmPassword { get; set; }
 
 		public double? salary { get; set; }
